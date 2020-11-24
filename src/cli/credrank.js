828c0224f525b3265f92c3d179fa53798b09eaf8
Thena Seer <seerthena@gmail.com>
2020-11-23 21:28:08 -0800

    CLI: Add -d diff flag to credrank command

--- src/cli/credrank.js
+++ src/cli/credrank.js
@@ -7,12 +7,13 @@ import {sum} from "d3-array";
 
 import sortBy from "../util/sortBy";
 import {credrank} from "../core/credrank/compute";
-import {CredGraph} from "../core/credrank/credGraph";
+import {CredGraph, type Participant} from "../core/credrank/credGraph";
 import {LoggingTaskReporter} from "../util/taskReporter";
 import {computeBonusMinting, createBonusGraph} from "../core/bonusMinting";
 import type {Command} from "./command";
-import {loadInstanceConfig, prepareCredData} from "./common";
+import {loadInstanceConfig, prepareCredData, loadCredGraph} from "./common";
 import {merge} from "../core/weightedGraph";
+import {type Uuid} from "../util/uuid";
 
 function die(std, message) {
   std.err("fatal: " + message);
@@ -20,8 +21,19 @@ function die(std, message) {
 }
 
 const credrankCommand: Command = async (args, std) => {
-  if (args.length !== 0) {
-    return die(std, "usage: sourcecred credrank");
+  let shouldIncludeDiff = false;
+  const processedArgs = args.filter((arg) => {
+    switch (arg) {
+      case "-d":
+        shouldIncludeDiff = true;
+        return false;
+      default:
+        return true;
+    }
+  });
+
+  if (processedArgs.length !== 0) {
+    return die(std, "usage: sourcecred credrank [-d]");
   }
   const taskReporter = new LoggingTaskReporter();
   taskReporter.start("credrank");
@@ -44,13 +56,25 @@ const credrankCommand: Command = async (args, std) => {
   const credGraph = await credrank(combinedWeightedGraph, ledger);
   taskReporter.finish("run CredRank");
 
+  if (shouldIncludeDiff) {
+    taskReporter.start("load prior graph");
+    try {
+      const priorCredGraph = await loadCredGraph(baseDir);
+      printCredDiffTable(credGraph, priorCredGraph);
+    } catch {
+      console.log("Could not load existing credGraph.json. Skipping diff.");
+      printCredSummaryTable(credGraph);
+    }
+    taskReporter.finish("load prior graph");
+  } else {
+    printCredSummaryTable(credGraph);
+  }
+
   taskReporter.start("write cred graph");
   const cgJson = stringify(credGraph.toJSON());
   const outputPath = pathJoin(baseDir, "output", "credGraph.json");
   await fs.writeFile(outputPath, cgJson);
   taskReporter.finish("write cred graph");
-
-  printCredSummaryTable(credGraph);
 
   taskReporter.finish("credrank");
   return 0;
@@ -71,6 +95,40 @@ function printCredSummaryTable(credGraph: CredGraph) {
     )}% |`;
   }
   sortedParticipants.slice(0, 20).forEach((n) => console.log(row(n)));
+}
+
+function printCredDiffTable(credGraph: CredGraph, priorCredGraph: CredGraph) {
+  console.log(`# Top Participants By New Cred`);
+  const priorParticipants: Map<Uuid, Participant> = new Map();
+  for (const participant of priorCredGraph.participants())
+    priorParticipants.set(participant.id, participant);
+
+  const credParticipants = Array.from(credGraph.participants());
+  const sortedParticipants = sortBy(credParticipants, (p) => -p.cred);
+  function row({cred, description, id}) {
+    const prior = priorParticipants.get(id);
+    if (!prior)
+      throw `Participant [${description}, ${id}] exists in the new scores but not in the old. Rerun without -d to refresh.`;
+    const percentageChange = (100 * (cred - prior.cred)) / prior.cred;
+    const percentageChangeStr =
+      percentageChange > 10000
+        ? ">10,000%"
+        : `${percentageChange > 0 ? "+" : ""}${percentageChange.toFixed(1)}%`;
+    return {
+      "Name": description,
+      "Prior Cred": prior.cred.toFixed(1),
+      "New Cred": cred.toFixed(1),
+      "% Change": percentageChangeStr,
+    };
+  }
+  type Row = {
+    "Name": string,
+    "Prior Cred": string,
+    "New Cred": string,
+    "% Change": string,
+  };
+  const rows: Array<Row> = sortedParticipants.slice(0, 20).map((n) => row(n));
+  console.table(rows);
 }
 
 export default credrankCommand;
